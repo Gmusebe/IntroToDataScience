a51b830ebc7039163aa442fa5083c4a3a1ce8bb2
Musebe Ivan <musebeivan@gmail.com>
2020-05-15 06:56:13 -0700

    Re-coded Syntax

--- TitanicDataAnalysis_Video1.R
+++ TitanicDataAnalysis_Video1.R
@@ -29,24 +29,31 @@ train <- read.csv("train.csv", header = TRUE)
 test <- read.csv("test.csv", header = TRUE)
 
 # Add a "Survived" variable to the test set to allow for combining data sets
-test.survived <- data.frame(survived = rep("None", nrow(test)), test[,])
+library(tibble)
+test <- add_column(test, data.frame(Survived = rep("None", nrow(test))), .after = 1)
 
 # Combine data sets
-data.combined <- rbind(train, test.survived)
+data.combined <- rbind(train, test)
 
 # A bit about R data types (e.g., factors)
 str(data.combined)
 
-data.combined$survived <- as.factor(data.combined$survived)
-data.combined$pclass <- as.factor(data.combined$pclass)
+data.combined$Survived <- as.factor(data.combined$Survived)
+data.combined$Pclass <- as.factor(data.combined$Pclass)
+data.combined$Name <- as.factor(data.combined$Name)
 
+# Check  for the missing data
+visdat::vis_dat(data.combined)
+#  Check the measure of data missing
+naniar::vis_miss(data.combined)
 
 # Take a look at gross survival rates
-table(data.combined$survived)
+library(epiDisplay)
+tab1(data.combined$Survived, col=c("chocolate","brown1","brown4"))
 
 
 # Distribution across classes
-table(data.combined$pclass)
+tab1(data.combined$Pclass, col=c("chocolate","brown1","brown4"))
 
 
 # Load up ggplot2 package to use for visualizations
@@ -54,8 +61,8 @@ library(ggplot2)
 
 
 # Hypothesis - Rich folks survived at a higer rate
-train$pclass <- as.factor(train$pclass)
-ggplot(train, aes(x = pclass, fill = factor(survived))) +
+train$Pclass <- as.factor(train$Pclass)
+ggplot(train, aes(x = Pclass, fill = factor(Survived))) +
   geom_bar() +
   xlab("Pclass") +
   ylab("Total Count") +
@@ -63,77 +70,46 @@ ggplot(train, aes(x = pclass, fill = factor(survived))) +
 
 
 # Examine the first few names in the training data set
-head(as.character(train$name))
+train$Name <- as.character(train$Name)
+head(train$Name)
 
 
 # How many unique names are there across both train & test?
-length(unique(as.character(data.combined$name)))
+length(unique(data.combined$Name))
 
 
 # Two duplicate names, take a closer look
 # First, get the duplicate names and store them as a vector
-dup.names <- as.character(data.combined[which(duplicated(as.character(data.combined$name))), "name"])
-
-
-# Next, take a look at the records in the combined data set
-data.combined[which(data.combined$name %in% dup.names),]
+dup.names <- as.character(data.combined[which(duplicated(data.combined$Name)), "Name"])
+# There are no duplicates in the data
 
 
 # What is up with the 'Miss.' and 'Mr.' thing?
+# Expand upon the realtionship between `Survived` and `Pclass` by adding the new `Title` variable to the
+# data set and then explore a potential 3-dimensional relationship.
 library(stringr)
+Title <- as.factor(str_extract(string = data.combined$Name, pattern = "(Mr|Master|Mrs|Miss)\\."))
+data.combined <- add_column(data.combined, Title, .after = 3)
 
-
-# Any correlation with other variables (e.g., sibsp)?
-misses <- data.combined[which(str_detect(data.combined$name, "Miss.")),]
+# Any correlation with other variables (e.g., sibsp[Siblings/Spouse])?
+misses <- data.combined[which(str_detect(data.combined$Name, "Miss.")),]
 misses[1:5,]
 
 
 # Hypothesis - Name titles correlate with age
-mrses <- data.combined[which(str_detect(data.combined$name, "Mrs.")), ]
+mrses <- data.combined[which(data.combined$Title == "Mrs."), ]
 mrses[1:5,]
 
 
 # Check out males to see if pattern continues
-males <- data.combined[which(data.combined$sex == "male"), ]
+males <- data.combined[which(data.combined$Sex == "male"), ]
 males[1:5,]
-
-
-# Expand upon the realtionship between `Survived` and `Pclass` by adding the new `Title` variable to the
-# data set and then explore a potential 3-dimensional relationship.
-
-# Create a utility function to help with title extraction
-# NOTE - Using the grep function here, but could have used the str_detect function as well.
-extractTitle <- function(name) {
-  name <- as.character(name)
-  
-  if (length(grep("Miss.", name)) > 0) {
-    return ("Miss.")
-  } else if (length(grep("Master.", name)) > 0) {
-    return ("Master.")
-  } else if (length(grep("Mrs.", name)) > 0) {
-    return ("Mrs.")
-  } else if (length(grep("Mr.", name)) > 0) {
-    return ("Mr.")
-  } else {
-    return ("Other")
-  }
-}
-
-
-# NOTE - The code below uses a for loop which is not a very R way of
-#        doing things
-titles <- NULL
-for (i in 1:nrow(data.combined)) {
-  titles <- c(titles, extractTitle(data.combined[i,"name"]))
-}
-data.combined$title <- as.factor(titles)
-
 
 # Since we only have survived lables for the train set, only use the
 # first 891 rows
-ggplot(data.combined[1:891,], aes(x = title, fill = survived)) +
+ggplot(data.combined[1:891,], aes(x = Title, fill = Survived)) +
   geom_bar() +
-  facet_wrap(~pclass) + 
+  facet_wrap(~Pclass) + 
   ggtitle("Pclass") +
   xlab("Title") +
   ylab("Total Count") +
